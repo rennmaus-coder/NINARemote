c9f35ffa7d20958e78105aab55e907fb7dd58e4b
rennmaus-coder <christian@palm-family.de>
2022-11-07 14:06:56 +0100

    Projektdateien hinzufügen.

---
+++ NINARemote/NINARemote/Views/LoginPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using NINARemote.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NINARemote.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new LoginViewModel();
+        }
+    }
+}
